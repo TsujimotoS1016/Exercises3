df883c521fea8b88b1242c999a3da3bc0a30f38a
TsujimotoS1016 <tsujimotos1016@gmail.com>
2024-06-28 17:58:00 +0900

    Add files via upload

---
+++ HeadsorTailgame.py
@@ -0,0 +1,20 @@
+import random
+
+print('Tossing a coin...')
+Head = 0
+Tail = 0
+for i in range(0,3):
+    num = random.randint(0,1)
+    if(num):
+        value = 'Heads'
+        Head += 1
+    else:
+        value = 'Tails'
+        Tail +=1
+    print('round',i+1,':',value)
+print('Heads:',Head,'Tails:',Tail)
+if Head > Tail:
+    victory = 'You won'
+else:
+    victory = 'You lost'
+print(victory)
